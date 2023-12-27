c46f396bc3b888931cc82ddb82eca43e4c8b1df5
Nithya AJ <aj7nithya@gmail.com>
2023-12-27 20:38:00 +0530

    array slice

---
+++ react_coding/src/JavaScript/arraySlice.js
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30, 40, 50]
+
+const middleThree = numbers.slice(1, 4)
+console.log(middleThree)
+
+const numbs = numbers.slice()
+console.log(numbs)
+const numbs1 = numbers.slice(1)
+console.log(numbs1)
+const numbs2 = numbers.slice(-1)
+console.log(numbs2)
+const numbs3 = numbers.slice(-2)
+console.log(numbs3)
